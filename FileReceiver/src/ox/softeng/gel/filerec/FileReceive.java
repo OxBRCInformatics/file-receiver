07cfbb817c1dba68b47b9f1b2252cd6325031992
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-03-13 21:57:55 +0000

    Hopefully make less resource intensive and also allow for subfolders
    And add extra rabbitmq message properties set

--- FileReceiver/src/ox/softeng/gel/filerec/FileReceive.java
+++ FileReceiver/src/ox/softeng/gel/filerec/FileReceive.java
@@ -1,57 +1,57 @@
 package ox.softeng.gel.filerec;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import ox.softeng.gel.filerec.config.Config;
 import ox.softeng.gel.filerec.config.Context;
 import ox.softeng.gel.filerec.config.Folder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class FileReceive {
 
-	public static void main(String[] args) throws JAXBException, IOException, InterruptedException {
-		if(args.length < 5)
-		{
-			System.err.println("Usage: FileReceive configFile queueHost exchangeName burstQueue refreshTime(s)");
-			System.exit(0);
-		}
-		String configFilename = args[0];
-		String queueHost = args[1];
-		String exchangeName = args[2];
-		String burstQueue = args[3];
-		Long refreshTime = Long.parseLong(args[4]);
-		System.err.println("Using config file: " + configFilename);
-		System.err.println("Using Queue Host: " + queueHost);
-		System.err.println("Using Exchange Name: " + exchangeName);
-		System.err.println("Using BuRST Queue: " + burstQueue);
-		System.err.println("Using Refresh Time: " + refreshTime + " seconds");
-		refreshTime = refreshTime * 1000; // Convert to ms
+    public static void main(String[] args) throws JAXBException, IOException, InterruptedException, TimeoutException {
+        if (args.length < 5) {
+            System.err.println("Usage: FileReceive configFile queueHost exchangeName burstQueue refreshTime(s)");
+            System.exit(0);
+        }
+        String configFilename = args[0];
+        String queueHost = args[1];
+        String exchangeName = args[2];
+        String burstQueue = args[3];
+        Long refreshTime = Long.parseLong(args[4]);
+        System.err.println("Using config file: " + configFilename);
+        System.err.println("Using Queue Host: " + queueHost);
+        System.err.println("Using Exchange Name: " + exchangeName);
+        System.err.println("Using BuRST Queue: " + burstQueue);
+        System.err.println("Using Refresh Time: " + refreshTime + " seconds");
+        refreshTime = refreshTime * 1000; // Convert to ms
 
-		JAXBContext jc = JAXBContext.newInstance(Config.class);
+        JAXBContext jc = JAXBContext.newInstance(Config.class);
 
         Unmarshaller unmarshaller = jc.createUnmarshaller();
         File xml = new File(configFilename);
         Config config = (Config) unmarshaller.unmarshal(xml);
-        
-        for(Context c : config.getContext())
-        {
-        	for(Folder f : c.getFolder())
-        	{
-        		FolderMonitor fm = new FolderMonitor(c.getPath(),f, queueHost, exchangeName, burstQueue, refreshTime);
-        		//Touch tm = new Touch(Paths.get(c.getPath() + "/" + f.getFolderPath()));
-        		new Thread(fm).start();
-        		//new Thread(tm).start();
-        		
-        		System.out.println("Started process for folder " + c.getPath() + "/" + f.getFolderPath());
-        	}
+
+        // Only create 1 connection from the program, use 1 channel per monitor.
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(queueHost);
+        Connection connection = factory.newConnection();
+
+        for (Context c : config.getContext()) {
+            for (Folder f : c.getFolder()) {
+                FolderMonitor fm = new FolderMonitor(connection, c.getPath(), f, exchangeName, burstQueue, refreshTime);
+                //Touch tm = new Touch(Paths.get(c.getPath() + "/" + f.getFolderPath()));
+                new Thread(fm).start();
+                //new Thread(tm).start();
+
+                System.out.println("Started process for folder " + c.getPath() + "/" + f.getFolderPath());
+            }
         }
-
-		
-
-	}
-
+    }
 }
--- FileReceiver/src/ox/softeng/gel/filerec/FolderMonitor.java
+++ FileReceiver/src/ox/softeng/gel/filerec/FolderMonitor.java
@@ -1,11 +1,25 @@
 package ox.softeng.gel.filerec;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import ox.softeng.burst.domain.Severity;
+import ox.softeng.burst.services.MessageDTO;
+import ox.softeng.burst.services.MessageDTO.Metadata;
+import ox.softeng.gel.filerec.config.Folder;
+import ox.softeng.gel.filerec.config.Header;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -15,241 +29,243 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeoutException;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-
-import ox.softeng.gel.filerec.config.Folder;
-import ox.softeng.gel.filerec.config.Header;
-
-import ox.softeng.burst.domain.Severity;
-
-import ox.softeng.burst.services.MessageDTO;
-import ox.softeng.burst.services.MessageDTO.Metadata;
-
 public class FolderMonitor implements Runnable {
 
-	String contextPath;
-	Path dir;
-        Path moveDestination;
-	String dirName;
-        String moveDestinationName;
-	Folder folder;
-	
-	String queueHost; // "192.168.99.100"
-	String exchangeName; // "Carfax"
-	String burstQueue; // "noaudit.burst"
-	Long refreshTime;
-	
-	HashMap<String, Long> fileSizes;
-	HashMap<String, Long> lastModified;
-	HashMap<String, Long> timeDiscovered;
+    private String contextPath;
+    private Path dir;
+    private Folder folder;
 
-	JAXBContext burstMessageContext;
-    
-	
-	public FolderMonitor(String contextPath, Folder folder, String queueHost, String exchangeName, String burstQueue, Long refreshTime)
-	{
-		this.contextPath = contextPath;
-		dirName = contextPath + "/" + folder.getFolderPath();
-		dir = Paths.get(dirName.replace("//", "/"));
-                
-                moveDestinationName = contextPath + "/" + folder.getMoveDestination();
-		moveDestination = Paths.get(moveDestinationName.replace("//", "/"));
-                
-		this.folder = folder;
-		
-		this.queueHost = queueHost; // "192.168.99.100"
-		this.exchangeName = exchangeName; // "Carfax"
-		this.burstQueue = burstQueue; // "noaudit.burst"
-		this.refreshTime = refreshTime;
-		fileSizes = new HashMap<String, Long>();
-		lastModified = new HashMap<String, Long>();
-		timeDiscovered = new HashMap<String, Long>();
-		
-		try {
-			burstMessageContext = JAXBContext.newInstance(MessageDTO.class);
-		} catch (JAXBException e) {
-                    System.err.println("FolderMonitor:constructor:-");
-			e.printStackTrace();
-		}
-	}
-	
-	
-	@Override
-	public void run() {
-		
-		try {
-                    if (!Files.exists(dir)) {
-                        System.out.println("Folder not exists: " + dir);
-                        System.out.println("Creating folder: " + dir);
-                        Files.createDirectories(dir);
+    private String exchangeName; // "Carfax"
+    private String burstQueue; // "noaudit.burst"
+    private Long refreshTime;
+
+    private HashMap<String, Long> fileSizes;
+    private HashMap<String, Long> lastModified;
+    private HashMap<String, Long> timeDiscovered;
+
+    private JAXBContext burstMessageContext;
+
+    private Channel channel;
+    private Connection connection;
+
+
+    public FolderMonitor(Connection connection, String contextPath, Folder folder, String exchangeName, String burstQueue,
+                         Long refreshTime) {
+
+        this.connection = connection;
+        this.contextPath = contextPath;
+        String dirName = contextPath + "/" + folder.getFolderPath();
+        dir = Paths.get(dirName.replace("//", "/"));
+
+        String moveDestinationName = contextPath + "/" + folder.getMoveDestination();
+        Path moveDestination = Paths.get(moveDestinationName.replace("//", "/"));
+
+        this.folder = folder;
+
+        this.exchangeName = exchangeName; // "Carfax"
+        this.burstQueue = burstQueue; // "noaudit.burst"
+        this.refreshTime = refreshTime;
+        fileSizes = new HashMap<>();
+        lastModified = new HashMap<>();
+        timeDiscovered = new HashMap<>();
+
+        try {
+            burstMessageContext = JAXBContext.newInstance(MessageDTO.class);
+        } catch (JAXBException e) {
+            System.err.println("FolderMonitor:constructor:-");
+            e.printStackTrace();
+        }
+
+        try {
+            if (!Files.exists(dir)) {
+                System.out.println("Folder not exists: " + dir);
+                System.out.println("Creating folder: " + dir);
+                Files.createDirectories(dir);
+            }
+            if (!Files.exists(moveDestination)) {
+                System.out.println("Folder not exists: " + dir);
+                System.out.println("Creating folder: " + dir);
+                Files.createDirectories(moveDestination);
+            }
+            System.out.println("dir type: " + Files.getFileStore(dir).type());
+        } catch (IOException e2) {
+            System.err.println("FolderMonitor:run:-");
+            e2.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            // Only create the channel once as we only need 1 and creation is expensive
+            // Also allows the loop to run as long as the channel is open, if the channel closes then the program
+            // ends as no point running if the channel is closed
+            channel = connection.createChannel();
+            channel.exchangeDeclare(exchangeName, "topic", true);
+
+            while (channel.isOpen()) {
+                Long currentTime = System.currentTimeMillis();
+                // First we'll go through the files that we've already found
+
+                // Keep a copy of the keyset so that we can modify the underlying hashset while iterating.
+                Set<String> fileNames = new HashSet<>(timeDiscovered.keySet());
+                for (String fileName : fileNames) {
+                    //System.out.println("Re-examining file: " + fileName);
+
+                    File f = new File(fileName);
+                    // If the file no-longer exists, then remove it
+                    if (!f.exists() || f.isDirectory()) {
+                        //System.out.println("file no-longer exists!");
+                        fileSizes.remove(fileName);
+                        lastModified.remove(fileName);
+                        timeDiscovered.remove(fileName);
+                    } else {
+                        Long thisFileLastModified = f.lastModified();
+                        Long thisFileSize = f.length();
+
+                        // if it's been modified, then update it
+                        if (!thisFileLastModified.equals(lastModified.get(fileName))
+                            || !thisFileSize.equals(fileSizes.get(fileName))) {
+                            //System.out.println("File modified since last examined.");
+                            fileSizes.put(fileName, thisFileSize);
+                            lastModified.put(fileName, thisFileLastModified);
+                            timeDiscovered.put(fileName, currentTime);
+
+                        } else // It's the same file as we've seen before...
+                        {
+                            // Only consider it if it has been there for a suitable duration
+                            if (timeDiscovered.get(fileName) + refreshTime < currentTime) {
+                                //System.out.println("File not modified.");
+                                try {
+                                    handleNewFile(f.getName());
+                                    fileSizes.remove(fileName);
+                                    lastModified.remove(fileName);
+                                    timeDiscovered.remove(fileName);
+                                } catch (IOException | TimeoutException | JAXBException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
                     }
-                    if (!Files.exists(moveDestination)) {
-                        System.out.println("Folder not exists: " + dir);
-                        System.out.println("Creating folder: " + dir);
-                        Files.createDirectories(moveDestination);
-                    }
-                    System.out.println("dir type: " + Files.getFileStore(dir).type());
-		} catch (IOException e2) {
-                    System.err.println("FolderMonitor:run:-");
-                    e2.printStackTrace();
-		}
-		while(true)
-		{
-			Long currentTime = System.currentTimeMillis();
-			// First we'll go through the files that we've already found
-			
-			// Keep a copy of the keyset so that we can modify the underlying hashset while iterating.
-			Set<String> fileNames = new HashSet<String>(timeDiscovered.keySet());
-			for(String fileName : fileNames)
-			{
-				//System.out.println("Re-examining file: " + fileName);
-				
-				File f = new File(fileName);
-				// If the file no-longer exists, then remove it
-				if(!f.exists() || f.isDirectory())
-				{
-					//System.out.println("file no-longer exists!");
-					fileSizes.remove(fileName);
-					lastModified.remove(fileName);
-					timeDiscovered.remove(fileName);
-				}
-				else{
-					Long thisFileLastModified = f.lastModified();
-					Long thisFileSize = f.length();
-					
-					// if it's been modified, then update it
-					if(!thisFileLastModified.equals(lastModified.get(fileName))
-						|| !thisFileSize.equals(fileSizes.get(fileName)))
-					{
-						//System.out.println("File modified since last examined.");
-						fileSizes.put(fileName,  thisFileSize);
-						lastModified.put(fileName, thisFileLastModified);
-						timeDiscovered.put(fileName, currentTime);
-					
-					}
-					else // It's the same file as we've seen before...
-					{
-						// Only consider it if it has been there for a suitable duration
-						if(timeDiscovered.get(fileName) + refreshTime < currentTime)
-						{
-							//System.out.println("File not modified.");
-							try {
-								handleNewFile(f.getName());
-								fileSizes.remove(fileName);
-								lastModified.remove(fileName);
-								timeDiscovered.remove(fileName);
-							} catch (IOException | TimeoutException e) {
-								e.printStackTrace();
-							} catch (JAXBException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}						
-					}
-				}
-			}
+                }
 
-			// Now we'll look and see if there are any new files to add
-			for(File f : dir.toFile().listFiles())
-			{
-				String thisFileAbsolutePath = f.getAbsolutePath();
-				// If we've not seen this file before
-				if(!lastModified.containsKey(thisFileAbsolutePath))
-				{
-					Long thisFileLastModified = f.lastModified();
-					Long thisFileSize = f.length();
-					System.out.println("registering file: " + thisFileAbsolutePath);
-					
-					fileSizes.put(thisFileAbsolutePath,  thisFileSize);
-					lastModified.put(thisFileAbsolutePath, thisFileLastModified);
-					timeDiscovered.put(thisFileAbsolutePath, currentTime);
-				}
-			
-			}
-			try {
-				// Sleep for a second
-				Thread.sleep(1000L);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}					
-			
-		} 
-        
+                // Handle recursive/sub folders
+                scanDirectoryFiles(dir.toFile().listFiles(), currentTime);
 
-	}
-		
-	private void handleNewFile(String filename) throws IOException, TimeoutException, JAXBException
-	{
-		String fullPath =  contextPath + folder.getFolderPath() + "/" + filename;
-		
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost(queueHost);
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
-	    
-	    channel.exchangeDeclare(exchangeName, "topic", true);
-	    byte[] message = Files.readAllBytes(Paths.get(fullPath));
-	    
-	    Map<String,Object> headerMap = new HashMap<String, Object>();
-	    headerMap.put("filename",filename);
-	    headerMap.put("directory",contextPath + folder.getFolderPath());
-	    headerMap.put("receivedDateTime",OffsetDateTime.now(ZoneId.systemDefault()).toString());
-	    for(Header h : folder.getHeaders().getHeader())
-	    {
-	    	headerMap.put(h.getKey(), h.getValue());
-	    }
-	    AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties().builder();
-	    builder.headers(headerMap);
-	    
-	    channel.basicPublish(exchangeName, folder.getQueueName(), builder.build(), message);
-	    //String burstMessage = "File received";
+                try {
+                    // Sleep for a second
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-	    
-	    channel.basicPublish(exchangeName, burstQueue, builder.build(), getSuccessMessage(filename));
-	    //System.out.println(" [x] Sent '" + message + "'");
-		
-	    String newPath = contextPath + folder.getMoveDestination() + "/" + filename;
-		File file = new File(fullPath);
-		file.renameTo(new File(newPath));
-		channel.close();
-		connection.close();
-	    
-		System.out.println("Processed: " + fullPath);
-	}
-	
-	
-	private byte[] getSuccessMessage(String filename) throws JAXBException
-	{
-	    MessageDTO burstMessage = new MessageDTO();
-	    burstMessage.setDateTimeCreated(LocalDateTime.now());
-	    burstMessage.setSeverity(Severity.NOTICE);
-	    burstMessage.setSource("Sample tracking system");
-	    String GMCName = "Unknown GMC";
-	    for(Header h : folder.getHeaders().getHeader())
-	    {
-	    	if("GMC".equalsIgnoreCase(h.getKey()))
-	    	{
-	    		GMCName = h.getValue();
-	    		break;
-	    	}
-	    }
-	    burstMessage.setDetails("A file with the name \"" + filename + "\" has been uploaded for sample tracking by " + GMCName);
-	    burstMessage.addTopic("File Receipt");
-	    burstMessage.addMetadata(new Metadata("GMC", GMCName));
-	    burstMessage.addMetadata(new Metadata("File name", filename));
-	    
-	    Marshaller m = burstMessageContext.createMarshaller(); 
-	    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	    ByteArrayOutputStream bas = new ByteArrayOutputStream();
-	    m.marshal(burstMessage, bas );
-	    return bas.toByteArray();
-	}
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void scanDirectoryFiles(File[] files, Long currentTime) {
+        // Now we'll look and see if there are any new files to add
+        if (files == null || files.length == 0) return;
+
+        for (File f : files) {
+
+            if (f.isDirectory()) {
+                scanDirectoryFiles(f.listFiles(), currentTime);
+            } else {
+                String thisFileAbsolutePath = f.getAbsolutePath();
+                // If we've not seen this file before
+                if (!lastModified.containsKey(thisFileAbsolutePath)) {
+                    Long thisFileLastModified = f.lastModified();
+                    Long thisFileSize = f.length();
+                    System.out.println("registering file: " + thisFileAbsolutePath);
+
+                    fileSizes.put(thisFileAbsolutePath, thisFileSize);
+                    lastModified.put(thisFileAbsolutePath, thisFileLastModified);
+                    timeDiscovered.put(thisFileAbsolutePath, currentTime);
+                }
+            }
+        }
+    }
+
+    private void handleNewFile(String filename) throws IOException, TimeoutException, JAXBException {
+        String fullPath = contextPath + folder.getFolderPath() + "/" + filename;
+
+        Path path = Paths.get(fullPath);
+
+        byte[] message = Files.readAllBytes(path);
+
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("filename", filename);
+        headerMap.put("directory", contextPath + folder.getFolderPath());
+        headerMap.put("receivedDateTime", OffsetDateTime.now(ZoneId.systemDefault()).toString());
+        for (Header h : folder.getHeaders().getHeader()) {
+            headerMap.put(h.getKey(), h.getValue());
+        }
+        AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties().builder();
+
+        // Set headers and other required properties
+        builder.headers(headerMap);
+        builder.appId("folder_monitor_" + contextPath);
+        builder.messageId(fullPath + "_" + headerMap.get("receivedDateTime"));
+        builder.timestamp(Date.from(OffsetDateTime.now(ZoneId.systemDefault()).toInstant()));
+        builder.type("file");
+        builder.contentType(determineContentType(filename));
+
+        // Send to folder queue
+        channel.basicPublish(exchangeName, folder.getQueueName(), builder.build(), message);
+        //String burstMessage = "File received";
+
+        // Send to burst
+        channel.basicPublish(exchangeName, burstQueue, builder.build(), getSuccessMessage(filename));
+        //System.out.println(" [x] Sent '" + message + "'");
+
+        String newPath = contextPath + folder.getMoveDestination() + "/" + filename;
+        File file = new File(fullPath);
+        file.renameTo(new File(newPath));
+
+        System.out.println("Processed: " + fullPath);
+    }
+
+    private String determineContentType(String filename) {
+        String ext = com.google.common.io.Files.getFileExtension(filename);
+
+        switch (ext.toLowerCase()) {
+            case "xml":
+                return "text/xml";
+            case "csv":
+                return "text/csv";
+            default:
+                return null;
+        }
+    }
+
+    private byte[] getSuccessMessage(String filename) throws JAXBException {
+        MessageDTO burstMessage = new MessageDTO();
+        burstMessage.setDateTimeCreated(LocalDateTime.now());
+        burstMessage.setSeverity(Severity.NOTICE);
+        burstMessage.setSource("Sample tracking system");
+        String GMCName = "Unknown GMC";
+        for (Header h : folder.getHeaders().getHeader()) {
+            if ("GMC".equalsIgnoreCase(h.getKey())) {
+                GMCName = h.getValue();
+                break;
+            }
+        }
+        burstMessage.setDetails("A file with the name \"" + filename + "\" has been uploaded for sample tracking by " + GMCName);
+        burstMessage.addTopic("File Receipt");
+        burstMessage.addMetadata(new Metadata("GMC", GMCName));
+        burstMessage.addMetadata(new Metadata("File name", filename));
+
+        Marshaller m = burstMessageContext.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        ByteArrayOutputStream bas = new ByteArrayOutputStream();
+        m.marshal(burstMessage, bas);
+        return bas.toByteArray();
+    }
 
 }
